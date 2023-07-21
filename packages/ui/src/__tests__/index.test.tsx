38da89bb8b2c5959d865611a818885d2d0f4ce39
Natchii59 <caron.nathan@hotmail.com>
2023-07-21 11:00:41 +0200

    packages: update latest

--- packages/ui/src/__tests__/index.test.tsx
+++ packages/ui/src/__tests__/index.test.tsx
@@ -1,13 +1,22 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Button } from '../components/ui/button'
+import { Avatar, Button } from '../components'
 
 describe('Button', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div')
     const root = createRoot(div)
     root.render(<Button />)
+    root.unmount()
+  })
+})
+
+describe('Avatar', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div')
+    const root = createRoot(div)
+    root.render(<Avatar />)
     root.unmount()
   })
 })

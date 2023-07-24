12bffb024b4b58254c89b27e3edb9fe9a11e1861
Natchii59 <caron.nathan@hotmail.com>
2023-07-24 15:08:54 +0200

    feat: add scroll area for sidebar

--- apps/client/src/components/sidebar/index.tsx
+++ apps/client/src/components/sidebar/index.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react'
 import Link from 'next/link'
+import { ScrollArea } from 'ui'
 
 import { AccountDropdown } from './account-dropdown'
 import { AccountDropdownLoading, SidebarUsersLoading } from './loading'
@@ -7,16 +8,18 @@ import { SidebarUsers } from './sidebar-users'
 
 export async function Sidebar() {
   return (
-    <nav className='flex min-h-screen flex-col gap-4 rounded-md border-r border-secondary p-2'>
+    <nav className='flex h-screen flex-col gap-4 rounded-md border-r border-secondary p-2'>
       <Link href='/' className='font-heading text-2xl'>
         Next Chat
       </Link>
 
-      <div className='flex flex-auto flex-col items-start gap-2'>
-        <Suspense fallback={<SidebarUsersLoading />}>
-          <SidebarUsers />
-        </Suspense>
-      </div>
+      <ScrollArea className='flex-auto'>
+        <div className='flex flex-col gap-2'>
+          <Suspense fallback={<SidebarUsersLoading />}>
+            <SidebarUsers />
+          </Suspense>
+        </div>
+      </ScrollArea>
 
       <Suspense fallback={<AccountDropdownLoading />}>
         <AccountDropdown />
--- apps/client/src/components/sidebar/sidebar-user-item.tsx
+++ apps/client/src/components/sidebar/sidebar-user-item.tsx
@@ -1,22 +1,28 @@
+'use client'
+
 import Link from 'next/link'
+import { useParams } from 'next/navigation'
 import { buttonVariants, cn } from 'ui'
 
 import { UserAvatar } from '@/components/user-avatar'
 
 interface SidebarUserProps {
   user: {
-    id: number
+    id: string
     name: string
   }
 }
 
 export function SidebarUserItem({ user }: SidebarUserProps) {
+  const params = useParams() as { chatId?: string }
+
   return (
     <Link
       href={`/chat/${user.id}`}
       className={cn(
         buttonVariants({ variant: 'ghost' }),
-        'h-auto w-full justify-start px-2 py-px'
+        'h-auto w-full justify-start px-2 py-px',
+        params.chatId === user.id && 'bg-accent'
       )}
     >
       <UserAvatar user={user} className='mr-2' />
